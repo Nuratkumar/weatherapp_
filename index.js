2c594344579e19783cfae93c83ad433d9f58971f
TARUN KUMAR PALAKA <148874364+Nuratkumar@users.noreply.github.com>
2024-02-17 20:26:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var inputvalue = document.querySelector('#cityinput');
+var btn = document.querySelector('#add');
+var city = document.querySelector('#citylayout');
+var description = document.querySelector('#description');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var apik = "28550c34e1428e98d7dbc6c61980e4fe";
+
+function conversion(val) {
+    return (val - 273).toFixed(3);
+}
+
+btn.addEventListener('click', function () {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&appid=' + apik)
+        .then(res => res.json())
+        .then(data => {
+            var nameval = data['name'];
+            var descrip = data['weather'][0]['description'];
+            var temperature = data['main']['temp'];
+            var windspeed = data['wind']['speed'];
+
+            city.innerHTML = "Weather of <span>" + nameval + "</span>";
+            temp.innerHTML = "Temperature: <span>" + conversion(temperature) + "°C</span>";
+            description.innerHTML = "Sky condition: <span>" + descrip + "</span>";
+            wind.innerHTML = "Wind speed: <span>" + windspeed + " km/h</span>";
+        })
+        .catch(err => alert('You entered the wrong city name'));
+});
